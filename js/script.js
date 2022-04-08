6d697b6723d21413a9a85af3c119c60291d07219
Aibek <ai.bek@mail.ru>
2022-04-08 19:02:56 +0600

    feat: add header and first screen

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-/*const burger = document.querySelector('.header__burger');
+const burger = document.querySelector('.header__burger');
 const menu = document.querySelector('.header__menu');
 const menuLink = document.querySelectorAll('.menu__link');
 const block = document.createElement('div');
-const logo = document.querySelector('.header__logo');
 document.body.append(block);
 
 burger.addEventListener('click', () => {
@@ -10,7 +9,6 @@ burger.addEventListener('click', () => {
    menu.classList.toggle('active');
    document.body.classList.toggle('lock');
    block.classList.toggle('block');
-   logo.classList.toggle('inactive')
 })
 
 menuLink.forEach((link, index) => link.addEventListener('click', () => {
@@ -18,11 +16,8 @@ menuLink.forEach((link, index) => link.addEventListener('click', () => {
    menu.classList.remove('active');
    document.body.classList.remove('lock');
    block.classList.remove('block');
-   logo.classList.remove('inactive');
-
 }))
 
-*/
 function ibg() {
 
    let ibg = document.querySelectorAll(".ibg");
